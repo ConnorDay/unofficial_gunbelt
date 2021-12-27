307ec0b7bfd10a11a58c914a7c53aa391238137b
Connor Day <connor.t.day@gmail.com>
2021-12-27 17:58:20 -0500

    add edit button and reworked how App was structured

--- gunbelt/src/App.js
+++ gunbelt/src/App.js
@@ -7,42 +7,43 @@ class App extends React.Component{
     constructor( props ){
         super(props);
         this.state = {
-            characterId: null
+            characterId: null,
+            editMode: false
         }
     }
 
     setCharacterId( id ){
         this.setState({characterId: id});
     }
+    toggleEditMode(){
+        const {editMode} = this.state;
+        this.setState({editMode: !editMode});
+    }
 
     render() {
-        const {characterId} = this.state;
-        if (characterId === null){
-            return( 
-                <div className="App">
-                    <header className="App-header">
-                        <CharacterSelect idUpdater={ (id) => this.setCharacterId(id)}/>
-                    </header>
-                    <div className='App-body'>
-                        <p className='App-body-center'>
-                            Please select a character :)
-                        </p>
-                    </div>
-                </div>
-            );
-        }
-        return (
+        const {characterId, editMode} = this.state;
+        console.log(editMode);
+        return( 
             <div className="App">
                 <header className="App-header">
                     <CharacterSelect idUpdater={ (id) => this.setCharacterId(id)}/>
+                    <button onClick={() => this.toggleEditMode()}>Edit</button>
                 </header>
                 <div className='App-body'>
-                    <div className='App-body-left'>
-                        <SkillTable characterId={characterId} />
-                    </div>
-                    <div className='App-body-center'>
-                        <Overview characterId={characterId} />
-                    </div>
+                    { characterId === null ?
+                        <p className='App-body-center'>
+                            Please select a character :)
+                        </p> 
+                        :
+                        <>
+                            <div className='App-body-left'>
+                                <SkillTable characterId={characterId} editMode={editMode}/>
+                            </div>
+                            <div className='App-body-center'>
+                                <Overview characterId={characterId} editMode={editMode}/>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         );
